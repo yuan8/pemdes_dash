da5b7143124f990918bc9191f307fbab32e2207b
yuan putra <bhalayuan@gmail.com>
2021-07-18 08:44:38 +0700

    dsds

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('integrasi:data')
-        ->everyFiveMinutes();
+        ->everyMinute();
+        // ->everyFiveMinutes();
                  // ->everyFifteenMinutes();
 
         $schedule->command('wa:blash')
